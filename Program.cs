3878a44688fc2d71a1e77da99a8d1b98dbc4c587
Robert Dettmann <implicate-x@wasm.page>
2022-05-16 22:40:25 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace MemoryTape
+{
+	/// <summary>
+	/// The program.
+	/// </summary>
+	internal class Program
+	{
+		/// <summary>
+		/// Mains the.
+		/// </summary>
+		static void Main()
+		{
+		}
+	}
+}
